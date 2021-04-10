cb6f7f68d4996e70ae6526cd8bca1ef86f1d46b1
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2021-04-09 21:05:15 -0400

    updating heroku script

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(express.static(__dirname + './dist'));
 // Start the app by listening on the default
 // Heroku port
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + './dist/current-portfolio/index.html'));
+  res.sendFile('./dist/current-portfolio/index.html');
 });
 app.listen(process.env.PORT || 8080);
