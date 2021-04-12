2dd047f81626717996f741823cccc291fd7c79c7
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2021-04-11 22:30:45 -0400

    fixed qoutes

--- src/app/components/qoutes/qoutes.component.ts
+++ src/app/components/qoutes/qoutes.component.ts
@@ -7,19 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QoutesComponent implements OnInit {
   qoute1 = {
-    text: `Our greatest weakness lies in giving up. The most certain way
-  to succeed is always to try just one more time.`,
+    text: `"Our greatest weakness lies in giving up. The most certain way
+  to succeed is always to try just one more time."`,
     author: `- Thomas A. Edison`,
   };
   qoute2 = {
-    text: `Success is no accident. It is hard work, \n perseverance,
+    text: `"Success is no accident. It is hard work, perseverance,
   learning, studying, sacrifice and most of all, love of what
-  you are doing or learning to do.`,
+  you are doing or learning to do."`,
     author: `- Pele`,
   };
   qoute3 = {
-    text: `Failure is only the opportunity to begin again, this time
-  more intelligently.`,
+    text: `"Failure is only the opportunity to begin again, this time
+  more intelligently."`,
     author: `- Mary Anne Radmacher`,
   };
 
